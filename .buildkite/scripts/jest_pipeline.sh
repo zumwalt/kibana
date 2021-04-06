eaf9eb2802b10a40a74cca082f573842282003bf
Tyler Smalley <tyler.smalley@elastic.co>
2021-04-06 15:45:49 -0700

    Change

--- .buildkite/scripts/jest_pipeline.sh
+++ .buildkite/scripts/jest_pipeline.sh
@@ -6,7 +6,7 @@ echo "steps:"
 
 find src packages x-pack -name jest.config.js | while read file; do
 cat << EOF
-  - label: ":jest:"
+  - label: ":jest: $file"
     command: ".ci/buildkite/jobs/jest.sh $file"
     agents:
       queue: jest
---
+++ .ci/buildkite/jobs/jest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export BUILD_TS_REFS_DISABLE=true
+
+.ci/buildkite/bootstrap.sh
+
+node --expose-gc ./node_modules/.bin/jest --logHeapUsage --runInBand --config $1
