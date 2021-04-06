6813627ee2737ca752858b3150eb79494a0256e7
Tyler Smalley <tyler.smalley@elastic.co>
2021-04-06 15:27:58 -0700

    Try not using Docker
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/jest_pipeline.sh
+++ .buildkite/scripts/jest_pipeline.sh
@@ -7,12 +7,10 @@ echo "steps:"
 find src packages x-pack -name jest.config.js | while read file; do
 cat << EOF
   - label: ":jest:"
-    command: "docker run -it \"gcr.io/elastic-kibana-184716/buildkite/ci/base:$BUILDKITE_COMMIT\" node --expose-gc ./node_modules/.bin/jest --logHeapUsage --runInBand --config $file"
+    command: ".ci/buildkite/jobs/jest.sh $file"
     agents:
       queue: jest
     artifact_paths: target/junit/**/*.xml
     timeout_in_minutes: 60
-    plugins:
-      - 'uber-workflow/run-without-clone':
 EOF
 done
